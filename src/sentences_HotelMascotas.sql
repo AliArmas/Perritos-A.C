57a1d0e9049ac4bec44981ee4e2d5a04da643630
AliArmas <aliarmas@tutanota.com>
2021-11-14 10:20:32 -0600

    add views html

--- src/sentences_HotelMascotas.sql
+++ src/sentences_HotelMascotas.sql
@@ -1,1 +1,6 @@
 SELECT * FROM Usuario;
+
+/*
+Sentencias para la base de datos para obtencion de datos sobre las tablas Usuario y de perros
+*/
+SELECT Usuario.nombre, Usuario.telefono, Perro.nombre,Perro.raza FROM Perro JOIN Usuario ON Perro.idPerro=Usuario.idUsuario'
