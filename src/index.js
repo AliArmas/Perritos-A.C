e6bdbb26d6e7b6a5c9192c27e9103ab18d2d8463
AliArmas <aliarmas@tutanota.com>
2021-11-26 11:06:12 -0600

    changes

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.set('json spaces', 2);
 
 //api URL
 app.set('url', '/api/v1/');
-app.set('urlAd','/admin/');
+app.set('urlAd','');
 
 //motor de plantilla
 app.set('view engine','ejs');
@@ -26,15 +26,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(rapidash());
 
-//rutas de pagina 
+//rutas
 app.use(require('./routes/index'));
 app.use(`${app.get('url')}mascota`,require('./routes/Mascotas/mascota'));
 app.use(`${app.get('url')}users`,require('./routes/users/user'));
-//rutas de administrador
-app.use("/dashboard",require('./routes/admin/admin'));
-app.use("/users",require("./routes/users/user"));
-app.use("/cats",require("./routes/cats/cat"));
-app.use("/dogs",require("./routes/dogs/dog"));
+app.use("/admin",require('./routes/admin/admin'));
 //ERROR 404
 app.use((req,res, next) => {
     res.status(404).sendFile(__dirname+"/views/pages/404.html");
